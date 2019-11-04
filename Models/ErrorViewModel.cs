148932a6c23526eb777d781d5a4b3804cbf29ae9
alumno informatica <alumno@ciis.usil.edu.pe>
2019-11-04 14:37:28 -0500

    version 1.0

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Proyecto_Programacion_Multiplataforma.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
